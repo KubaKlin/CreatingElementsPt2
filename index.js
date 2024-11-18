f4f3b1872a1be91b3d94cb0804cf57a3ccecf8be
Kuba Klin <poczta@kubaklin.pl>
2024-11-18 12:39:53 +0100

    feat: typos and fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const taskInput = document.querySelector('.add-task-input');
 const taskInputButton = document.querySelector('.add-task-button');
 const tasksWrapper = document.querySelector('.tasks-column');
 const errorLabel = document.querySelector('.add-input-error-label');
-errorLabel.innerText = 'Tast title cannot be empty';
+errorLabel.innerText = 'Task title cannot be empty';
 
 const firstColumn = document.querySelector('#first-column');
 const secondColumn = document.querySelector('#second-column');
@@ -11,13 +11,13 @@ const thirdColumn = document.querySelector('#third-column');
 
 taskInputButton.addEventListener('click', function() {
   let taskTitle = taskInput.value;
-  const singleTask = document.createElement('div');
-  singleTask.classList.add('single-task');
-  singleTask.innerText = taskTitle;
+  const taskCard = document.createElement('div');
+  taskCard.classList.add('single-task');
+  taskCard.innerText = taskTitle;
 
   const buttonsWrapper = document.createElement('div');
   buttonsWrapper.classList.add('task-button-wrapper');
-  singleTask.append(buttonsWrapper);
+  taskCard.append(buttonsWrapper);
 
   const leftArrowButton = document.createElement('button');
   leftArrowButton.classList.add('button-left-arrow');
@@ -32,39 +32,38 @@ taskInputButton.addEventListener('click', function() {
   buttonsWrapper.append(rightArrowButton);
 
   binButton.addEventListener('click', function() {
-    singleTask.remove();
+    taskCard.remove();
   });
 
   let columnCount = 1;
   rightArrowButton.addEventListener('click', function() {
-    singleTask.cloneNode(true);
-    singleTask.remove();
+    taskCard.cloneNode(true);
+    taskCard.remove();
     if (columnCount === 1) {
-      secondColumn.append(singleTask);
-      columnCount = ++columnCount;
+      secondColumn.append(taskCard);
+      columnCount += 1;
     } else if (columnCount === 2) {
-      thirdColumn.append(singleTask);
-      columnCount = ++columnCount;
+      thirdColumn.append(taskCard);
+      columnCount += 1;
     }
   })
 
   leftArrowButton.addEventListener('click', function() {
-    singleTask.cloneNode(true);
-    singleTask.remove();
+    taskCard.cloneNode(true);
+    taskCard.remove();
     if (columnCount === 2) {
-      firstColumn.append(singleTask);
-      columnCount = --columnCount;
+      firstColumn.append(taskCard);
+      columnCount -= 1;
     } else if (columnCount === 3) {
-      secondColumn.append(singleTask);
-      columnCount = --columnCount;
+      secondColumn.append(taskCard);
+      columnCount -= 1;
     }
   })
 
   if (taskTitle !== '') {
-    tasksWrapper.append(singleTask);
+    tasksWrapper.append(taskCard);
     taskInput.value = '';
     errorLabel.classList.remove('active');
-    errorLabel.innerText = 'Task title cannot be empty';
   } else {
     errorLabel.classList.add('active');
   }
