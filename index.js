aed18e002ead0442add1be33f360596def42e6f1
Kuba Klin <poczta@kubaklin.pl>
2024-11-20 16:24:50 +0100

    fix: functions reworks

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ const firstColumn = document.querySelector('#first-column');
 const secondColumn = document.querySelector('#second-column');
 const thirdColumn = document.querySelector('#third-column');
 
-
-taskInputButton.addEventListener('click', function() {
-  let taskTitle = taskInput.value;
-  const taskCard = document.createElement('div');
-  taskCard.classList.add('single-task');
-  taskCard.innerText = taskTitle;
-
+function getButtonsWrapper() {
   const buttonsWrapper = document.createElement('div');
   buttonsWrapper.classList.add('task-button-wrapper');
-  taskCard.append(buttonsWrapper);
 
   const leftArrowButton = document.createElement('button');
   leftArrowButton.classList.add('button-left-arrow');
@@ -31,40 +24,67 @@ taskInputButton.addEventListener('click', function() {
   rightArrowButton.classList.add('button-right-arrow');
   buttonsWrapper.append(rightArrowButton);
 
+  return buttonsWrapper;
+}
+
+function moveTaskForward(taskCard, currentColumnNumber, leftArrowButton, rightArrowButton) {
+  taskCard.cloneNode(true);
+  taskCard.remove();
+  if (currentColumnNumber === 1) {
+    secondColumn?.append(taskCard);
+    leftArrowButton?.classList.remove('hidden');
+  } else if (currentColumnNumber === 2) {
+    thirdColumn?.append(taskCard);
+    rightArrowButton?.classList.add('hidden');
+  }
+}
+
+function moveTaskBackward(taskCard, currentColumnNumber, leftArrowButton, rightArrowButton) {
+  taskCard.cloneNode(true);
+  taskCard.remove();
+  if (currentColumnNumber === 2) {
+    firstColumn?.append(taskCard);
+    leftArrowButton?.classList.add('hidden');
+  } else if (currentColumnNumber === 3) {
+    secondColumn?.append(taskCard);
+    rightArrowButton?.classList.remove('hidden');
+  }
+}
+
+taskInputButton?.addEventListener('click', function() {
+  taskTitle = taskInput?.value;
+  const taskCard = document.createElement('div');
+  const buttonsWrapper = getButtonsWrapper();
+  taskCard.classList.add('single-task');
+  taskCard.innerText = taskTitle;
+
+  taskCard.append(buttonsWrapper);
+
+  const binButton = buttonsWrapper.querySelector('.button-bin');
+  const rightArrowButton = buttonsWrapper.querySelector('.button-right-arrow');
+  const leftArrowButton = buttonsWrapper.querySelector('.button-left-arrow');
+  leftArrowButton?.classList.add('hidden');
+
   binButton.addEventListener('click', function() {
     taskCard.remove();
   });
 
-  let columnCount = 1;
+  let currentColumnNumber = 1;
   rightArrowButton.addEventListener('click', function() {
-    taskCard.cloneNode(true);
-    taskCard.remove();
-    if (columnCount === 1) {
-      secondColumn.append(taskCard);
-      columnCount += 1;
-    } else if (columnCount === 2) {
-      thirdColumn.append(taskCard);
-      columnCount += 1;
-    }
+    moveTaskForward(taskCard, currentColumnNumber, leftArrowButton, rightArrowButton);
+    currentColumnNumber += 1;
   })
 
   leftArrowButton.addEventListener('click', function() {
-    taskCard.cloneNode(true);
-    taskCard.remove();
-    if (columnCount === 2) {
-      firstColumn.append(taskCard);
-      columnCount -= 1;
-    } else if (columnCount === 3) {
-      secondColumn.append(taskCard);
-      columnCount -= 1;
-    }
+    moveTaskBackward(taskCard, currentColumnNumber, leftArrowButton, rightArrowButton);
+    currentColumnNumber -= 1;
   })
 
   if (taskTitle !== '') {
-    tasksWrapper.append(taskCard);
+    tasksWrapper?.append(taskCard);
     taskInput.value = '';
-    errorLabel.classList.remove('active');
+    errorLabel?.classList.remove('active');
   } else {
-    errorLabel.classList.add('active');
+    errorLabel?.classList.add('active');
   }
 });
